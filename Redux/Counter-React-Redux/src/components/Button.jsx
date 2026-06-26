9255f983c7c860cb1e576524043a102e0fa1ac7e
daemanas <daemanas17@gmail.com>
2026-06-26 18:05:05 +0530

    react-redux setup and mini project

--- Redux/Counter-React-Redux/src/components/Button.jsx
+++ Redux/Counter-React-Redux/src/components/Button.jsx
@@ -1,13 +1,70 @@
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+
 const Button = () =>{
+    const Dispach = useDispatch();
+
+    const InputElement = useRef();
+
+    const handleIncrement = () =>{
+        Dispach({ type : "INCREMENT"});
+    }
+
+    const handleDecrement =() =>{
+        Dispach({ type : "DECREMENT"});
+    }
+
+    const handleAdd = () => {
+        Dispach({type : "ADD", payload :{
+            num: InputElement.current.value,
+        }})
+        InputElement.current.value ="";
+    }
+
+    const handleSub = () => {
+        Dispach({type : "Substract", payload : {
+            num : InputElement.current.value,
+        } })
+    }
+
     return(
-        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center"> 
-            <button type="button" className="btn btn-primary">
-                +1
-            </button> 
-            <button type="button" className="btn btn-success">
-                -1
-            </button> 
-        </div>
+        <>
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center"> 
+                <button 
+                    type="button" 
+                    className="btn btn-primary"
+                    onClick={handleIncrement}>
+                        +1
+                </button> 
+                <button 
+                    type="button" 
+                    className="btn btn-success"
+                    onClick={handleDecrement}>
+                        -1
+                </button>
+            </div>
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center control-row">
+                <input
+                type="text"
+                placeholder="Enter Number" 
+                className="Number input"
+                ref={InputElement}
+                />
+
+                <button 
+                type="button" 
+                className="btn btn-danger rounded-pill px-3"
+                onClick={handleAdd}>
+                    ADD
+                </button> 
+                <button 
+                    type="button" 
+                    className="btn btn-danger rounded-pill px-3"
+                    onClick={handleSub}>
+                        SUBS
+                </button>
+            </div>
+        </>
     )
 }
 
--- Redux/Counter-React-Redux/src/components/Display.jsx
+++ Redux/Counter-React-Redux/src/components/Display.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux";
+
 const Display = () => {
+
+    const counter = useSelector((store) => store.counter)
     return <p className="lead mb-4">
-            Counter Valus :
+            Counter Valus :{ counter }
     </p>
 }
 
